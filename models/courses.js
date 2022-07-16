e4152cde8c445aceecee0fe240698a11eae10bdb
Anastasiia Abbasova <a.abbasova@dinarys.com>
2022-07-16 16:01:42 +0300

    Add other layout

---
+++ models/courses.js
@@ -0,0 +1,68 @@
+// const { v4: uuid } = require('uuid');
+const uuid = require('uuid');
+const fs = require('fs')
+const path = require('path')
+
+class Course {
+    constructor(title, price, img) {
+        this.title = title
+        this.price = price
+        this.img = img
+        this.id = uuid.v4()
+    }
+
+    toJSON() {
+        return {
+            title: this.title,
+            price: this.price,
+            img: this.img,
+            id: this.id
+        }
+    }
+
+    async save() {
+        const courses = await Course.getAll()
+        courses.push(this.toJSON())
+
+        console.log(path.join(__dirname, '..', 'data', 'courses.json'))
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                }
+            )
+        })
+    }
+
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(JSON.parse(content))
+                    }
+                }
+            )
+        })
+    }
+
+    static async getById(id){
+        const courses = await Course.getAll()    
+        return courses.find((course) => {
+            return course.id === id;
+        })
+    }
+}
+
+module.exports = Course
