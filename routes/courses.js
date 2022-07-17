8d66077cf4d48c15d48e3dfc56108a801575a123
Anastasiia Abbasova <a.abbasova@dinarys.com>
2022-07-17 15:01:47 +0300

    Add 'Edit course'

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,9 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const { empty } = require('uuidv4')
 const Course = require('../models/courses')
-const router = Router ()
+const router = Router()
 
-router.get('/', async (req, res) => {    
+router.get('/', async (req, res) => {
     const courses = await Course.getAll()
     res.render('courses', {
         title: 'Courses',
@@ -12,7 +12,27 @@ router.get('/', async (req, res) => {
     })
 })
 
-router.get('/:id', async (req, res) => {    
+router.get('/:id/edit', async (req, res) => {
+    if (!req.query.allow) {
+        return res.redirect('/')
+    }
+
+    const course = await Course.getById(req.params.id)
+
+    res.render('course-edit', {
+        title: `Edit ${course.title}`,
+        course
+    })
+})
+
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body)
+
+    res.redirect('/courses')
+})
+
+router.get('/:id', async (req, res) => {
     const course = await Course.getById(req.params.id)
 
     res.render('course', {
